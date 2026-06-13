da846a5ee245cc023b31e4b419873fa1745010e9
mallett <mallettwa@pella.com>
2026-06-13 15:44:23 -0300

    references notes/cleanup

--- 04_ownership/references/src/main.rs
+++ 04_ownership/references/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     references();
-    try_modify_borrowed_reference();
+    try_modify_borrowed_reference(); // you can't, unless mutable
     only_one_mut_ref();
     ref_scope_duration();
     // left off: https://doc.rust-lang.org/book/ch04-02-references-and-borrowing.html#dangling-references
@@ -24,9 +24,10 @@ fn calculate_length(s: &String) -> usize { // s reference to a string (borrowing
 // doesn't own it, so s is not dropped
 
 fn try_modify_borrowed_reference() {
-    // let s = String::from("hello");
-    // change(&s);
+    let s = String::from("hello");
+    change(&s);
 
+    // but, can us mutable reference and modify value
     let mut s = String::from("hello");
     change_mutable_ref(&mut s);
 }
@@ -34,7 +35,7 @@ fn try_modify_borrowed_reference() {
 // this won't work - can't modify a borrowed value
 // similar to immutible vars
 fn change(some_string: &String) {
-    // some_string.push_str(", world");
+    some_string.push_str(", world");
 }
 
 fn change_mutable_ref(some_string: &mut String) {
@@ -44,11 +45,13 @@ fn change_mutable_ref(some_string: &mut String) {
 fn only_one_mut_ref() {
     let mut s = String::from("hello");
 
-    // If have a mut ref, can't create other references until the mut ref is used
+    // If have a mut ref, can't create other references until the mut ref is out of scope
     let r1 = &mut s;
-    // let r2 = &mut s;
+    let r2 = &mut s; // can't create another
 
-    // println!("{r1}, {r2}");
+    println!("{r1}, {r2}");
+
+
 
 
     // But can use curly brackets to create new scope
@@ -62,14 +65,15 @@ fn only_one_mut_ref() {
 
 
 
-    // can't combine mutable & immutable refs
+
+    // can't combine mutable & immutable references of same value (same address)
     let mut my_str = String::from("hello");
 
     let ref1 = &my_str; // no problem
     let ref2 = &my_str; // no problem
-    // let ref3 = &mut my_str; // BIG PROBLEM
+    let ref3 = &mut my_str; // BIG PROBLEM
 
-    println!("{ref1}, {ref2}");
+    println!("{ref1}, {ref2}, {ref3}");
 }
 
 fn ref_scope_duration() {
@@ -78,8 +82,9 @@ fn ref_scope_duration() {
 
     let r1 = &s; // no problem
     let r2 = &s; // no problem
-    println!("{r1}, {r2}"); // used, out of scope now
+    println!("{r1}, {r2}"); 
+    // r1 & r2 used, out of scope now
 
-    let r3 = &mut s; // no problem
+    let r3 = &mut s; // no problem - can create mutable reference now
     println!("{r3}");
 }
