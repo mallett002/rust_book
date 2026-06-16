0915b5a51f9b4609346d1f76483846a18d6fe570
mallett <mallettwa@pella.com>
2026-06-16 10:57:54 -0300

    more slices

--- 04_ownership/slices/src/main.rs
+++ 04_ownership/slices/src/main.rs
@@ -42,7 +42,9 @@ fn main() {
     // s.clear(); // needs mutable reference
     println!("the first word is: {word}"); // immutable reference
 
-    // left off: https://doc.rust-lang.org/book/ch04-03-slices.html#string-literals-as-slices
+
+    string_literals_as_slices();
+    other_slices();
 }
 
 fn first_word(s: &String) -> usize {
@@ -57,7 +59,11 @@ fn first_word(s: &String) -> usize {
     s.len()
 }
 
-fn first_word_slices(s: &String) -> &str {
+// Was: "fn first_word_slices(s: &String) -> &str {", more flexible if it's a &str instead of &String
+// s: String -> coerces into &str
+// can pass in string literals
+// can pass any string slice
+fn first_word_slices(s: &str) -> &str { // use a string slice instead (&str)
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
@@ -67,4 +73,45 @@ fn first_word_slices(s: &String) -> &str {
     }
 
     &s[..]
+}
+
+fn string_literals_as_slices() {
+    // https://doc.rust-lang.org/book/ch04-03-slices.html#string-literals-as-slices
+    // string literals are slices
+    let my_str = "howdy"; // my_str is a &str
+    // &str is a slice (ref to portion or all of string data)
+
+
+    let my_string = String::from("hello world");
+    // first_word_slices works on slices of strings, whether partial or whole
+    let word = first_word_slices(&my_string[0..6]);
+    let word = first_word_slices(&my_string[..]);
+
+    // first_word_slices also works references to `String`s, which are equivalent to whole slices
+    // of `String`s
+    let word = first_word_slices(&my_string);
+
+
+    let my_string_literal = "hello world";
+    // first_word_slices works on slices of string literals, partial and whole
+    let word = first_word_slices(&my_string_literal[0..6]);
+    let word = first_word_slices(&my_string_literal[..]);
+
+    // because string literals are string slices, this works too
+    let word = first_word_slices(my_string_literal);
+
+
+    // String is like a notebook page you own — you can write on it,
+    // erase it, make it longer.
+    // &str is like someone handing you a sticky note with words already written on it 
+    // — you can read them, but you can't change or add to them.
+}
+
+fn other_slices() {
+    // slices work on things other than strings as well
+    // this array:
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
 }
