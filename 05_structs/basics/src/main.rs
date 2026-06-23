8710192b56e5991a5e73423772b928fd29fa5b2f
mallett <mallettwa@pella.com>
2026-06-23 19:18:48 -0300

    structs

--- 05_structs/basics/src/main.rs
+++ 05_structs/basics/src/main.rs
@@ -37,7 +37,9 @@ fn main() {
     );
 
     struct_update_syntax();
-    // left off: https://doc.rust-lang.org/book/ch05-01-defining-structs.html#creating-different-types-with-tuple-structs
+    diff_types_with_tuple_structs();
+
+    // left off here: https://doc.rust-lang.org/book/ch05-01-defining-structs.html#defining-unit-like-structs
 }
 
 fn build_user(username: String, email: String) -> User {
@@ -76,4 +78,20 @@ fn struct_update_syntax() {
     // println!("{}", other_jim.username); // can't do (data moved to other_jim_2)
     println!("{}", other_jim_2.username);
 
+}
+
+
+struct Color(i32, i32, i32);
+
+struct Point(i32, i32, i32);
+
+fn diff_types_with_tuple_structs() {
+    // https://doc.rust-lang.org/book/ch05-01-defining-structs.html#creating-different-types-with-tuple-structs
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    // destructuring tuple structs
+    let Point(x, y, z) = origin;
+
+    println!("{x}, {y}, {z}");
 }
