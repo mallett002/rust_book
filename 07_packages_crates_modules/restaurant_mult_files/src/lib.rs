fef3424cfec9a5a28ea5d2882893968696619691
mallett <mallettwa@pella.com>
2026-07-23 17:25:45 -0500

    left off

--- 07_packages_crates_modules/restaurant_mult_files/src/lib.rs
+++ 07_packages_crates_modules/restaurant_mult_files/src/lib.rs
@@ -5,3 +5,5 @@ pub use crate::front_of_house::hosting;
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
 }
+
+// TODO: left off: https://doc.rust-lang.org/book/ch08-00-common-collections.html
