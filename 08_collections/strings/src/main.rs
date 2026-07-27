e97f2e17de317a68c100d380c90693dda8cb6f5a
mallett <mallettwa@pella.com>
2026-07-27 17:13:22 -0500

    :broom:

--- 08_collections/strings/src/main.rs
+++ 08_collections/strings/src/main.rs
@@ -4,8 +4,8 @@ fn main() {
 
     // with initial data
     let data = "initial contents";
-    let s = data.to_string()
-    let s = "hello".to_string()
+    let s = data.to_string();
+    let s = "hello".to_string();
     let s = String::from(data);
 
     // TODO: left off https://doc.rust-lang.org/book/ch08-02-strings.html#listing-8-12
