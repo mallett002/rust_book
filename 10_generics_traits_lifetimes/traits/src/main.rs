894d64b9cc6a3f8b51b5de5c18973af3f064749f
mallett <mallettwa@pella.com>
2026-08-14 07:33:04 -0500

    starting lifetimes

---
+++ 10_generics_traits_lifetimes/traits/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Traits define functionality a type has.
+    // Traits allow us to share functionality with other types
+    // They are sort of like interfaces in other languages
+
+    println!("Hello, world!");
+
+    // TODO: left off starting traits `https://doc.rust-lang.org/book/ch10-02-traits.html`
+}
