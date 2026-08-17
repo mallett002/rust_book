067e2f43a02a78087d90ebb7691c5f7e4a5dcb33
mallett <mallettwa@pella.com>
2026-08-17 08:01:22 -0500

    traits

--- 10_generics_traits_lifetimes/aggregator/src/lib.rs
+++ 10_generics_traits_lifetimes/aggregator/src/lib.rs
@@ -1,3 +1,6 @@
+use std::fmt::Debug;
+use std::fmt::Display;
+
 // define a trait (interface)
 pub trait Summary {
     fn summarize_author(&self) -> String;
@@ -49,9 +52,56 @@ impl Summary for SocialPost {
     }
 }
 
+// Using traits as parameters
+// Accepts anything that implements the Summary trait
+fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// Trait bound syntax (longer form of the "traits as parameters")
+fn notify_2<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// allow 2 parameters that implement Summary
+fn notify_3(item1: &impl Summary, item2: &impl Summary) {
+    println!("Breaking news! {}", item1.summarize());
+    println!("Breaking news! {}", item2.summarize());
+}
+
+// Force both parameters to have the same type (have to use Trait Bound syntax)
+fn notify_4<T: Summary>(item1: &T, item2: &T) {
+    println!("Breaking news! {}", item1.summarize());
+    println!("Breaking news! {}", item2.summarize());
+}
+
+// Ensure parameter implements more than one trait
+fn notify_5(item: &(impl Summary + Display)) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// Ensure parameter implements more than one trait with Trait bounds
+fn notify_6<T: Summary + Display>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// Using multiple Trait Bounds with Where syntax
+// Cluttered and hard to read:
+fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {
+    5
+}
+
+// improved with Where syntax
+fn some_function2<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+    5
+}
+
 // Note - can only add a trait to a type if:
 // - The trait or the type are defined locally in your project
 // - Or both are defined locally.
 // If both are defined outside your project. You can't add a trait to a type
 // - "You can't add an externally defined trait to an externally defined type"
-// TODO: left off https://doc.rust-lang.org/book/ch10-02-traits.html#using-traits-as-parameters
