2a23704bc7d1709370b06db607256a296ed46837
mallett <mallettwa@pella.com>
2026-08-20 06:39:04 -0500

    lifetimes

--- 10_generics_traits_lifetimes/lifetimes/src/main.rs
+++ 10_generics_traits_lifetimes/lifetimes/src/main.rs
@@ -27,15 +27,29 @@ fn dangling_refs() {
     println!("a = {a}"); // problem - r 
 }
 
-fn longest(x: &str, y: &str) -> &str {
+
+
+// lifetime 'a: the returned ref will be valid as long as both params are valid (no dangling refs)
+// tells compiler: "reject code if return value could outlive any input"
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() { x } else { y }
 }
+// Lifetimes in longest func ^^
+// params will live at least as long as 'a (picks the shortest lived on and assigns to 'a)
+// return value will live at least as long as 'a
 
 fn generic_lifetimes_in_funcs() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
 
     let result = longest(string1.as_str(), string2);
-    // TODO: left off https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html#lifetime-annotation-syntax
+
+    // example lifetimes:
+    // &i32 -> just a reference
+    // &'a i32 -> ref with explicit lifetime
+    // &'a mut i32 -> mutable ref with explicit lifetime
+
+    // TODO: left off https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html#in-function-signatures
+    // Find text: "Let’s look at how the lifetime annotations restrict the longest function by passing in references that have different concrete lifetimes"
 }
 
