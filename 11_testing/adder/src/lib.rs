87916964d037a8cd4d9a4eda4cd2d3bbedd0af17
mallett <mallettwa@pella.com>
2026-08-25 06:48:19 -0500

    testing

--- 11_testing/adder/src/lib.rs
+++ 11_testing/adder/src/lib.rs
@@ -2,7 +2,17 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+fn add_two(n: u64) -> u64 {
+    n + 2
+}
+
+fn greeting(name: &str) -> String {
+    format!("Hello {name}!")
+}
+
+// needs to implement Debug & PartialEq for assert_eq!
 #[derive(Debug)]
+#[derive(PartialEq)] // 
 struct Rectangle {
     width: u32,
     height: u32,
@@ -11,6 +21,27 @@ struct Rectangle {
 impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+    }
+
+    fn new_with_height(height: u32) -> Self {
+        Self {
+            width: 30,
+            height,
+        }
+    }
+}
+
+struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {value}.");
+        }
+
+        Guess { value }
     }
 }
 
@@ -58,5 +89,51 @@ mod tests {
         assert!(!smaller.can_hold(&larger));
     }
 
-    // TODO: left off https://doc.rust-lang.org/book/ch11-01-writing-tests.html#testing-equality-with-assert_eq-and-assert_ne
+    #[test]
+    fn test_add_two() {
+        let result = add_two(5);
+
+        assert_eq!(result, 7);
+    }
+
+    #[test]
+    fn test_new_with_height() {
+        let height = 40;
+        let result = Rectangle::new_with_height(height);
+        let expected = Rectangle {
+            width: 30,
+            height,
+        };
+
+        // requires Debug & PartialEq for assert_eq!
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{result}`", 
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")] // matches substr of panic msg
+    fn greater_than_100() {
+        Guess::new(101);
+    }
+
+    // Can also use Result<T, E> on tests
+    #[test]
+    fn it_works() -> Result<(), String> {
+        let result = add(2, 2);
+
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("2 + 2 did not equal 4"))
+        }
+    }
 }
