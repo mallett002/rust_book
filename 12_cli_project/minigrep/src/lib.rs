2d323ebf8500d615dff3d0f3a63fd76fc6f057b7
mallett002 <mallett002@gmail.com>
2026-08-30 12:43:06 -0500

    wip passing test on minigrep

--- 12_cli_project/minigrep/src/lib.rs
+++ 12_cli_project/minigrep/src/lib.rs
@@ -1,7 +1,31 @@
 // 'a: "Returned &str in Vec are only valid as long as contents (param) are"
+// data returned by search will live as long as data in "contents" param
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
 }
 
-// TODO: left off https://doc.rust-lang.org/book/ch12-04-testing-the-librarys-functionality.html
+// TODO: left off https://doc.rust-lang.org/book/ch12-04-testing-the-librarys-functionality.html#iterating-through-lines-with-the-lines-method
 
+#[cfg(test)]
+mod tests {
+   use super::*; 
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
