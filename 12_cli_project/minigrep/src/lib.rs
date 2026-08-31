f1fffe9b207f8d307b808e1752f3045cd443c4a1
mallett002 <mallett002@gmail.com>
2026-08-31 06:18:35 -0500

    case insensitive search

--- 12_cli_project/minigrep/src/lib.rs
+++ 12_cli_project/minigrep/src/lib.rs
@@ -13,7 +13,16 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+    let query = query.to_lowercase();
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -27,15 +36,14 @@ mod tests {
 Rust:
 safe, fast, productive.
 Pick three.
-Duct tape.";
+Duct tape."; // shouldn't find "Duct" - case sensitive
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 
-    // TODO: left off (failing test) `https://doc.rust-lang.org/book/ch12-05-working-with-environment-variables.html#writing-a-failing-test-for-case-insensitive-search`
     #[test]
     fn case_insensitive() {
-        let query = "rUst";
+        let query = "rUsT";
         let contents = "\
 Rust:
 safe, fast, productive.
