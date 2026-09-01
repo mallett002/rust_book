3828d8960ec28457d59a953ccf3e92c082f4574c
mallett002 <mallett002@gmail.com>
2026-09-01 05:34:09 -0500

    override ignore case env with flag

--- 12_cli_project/minigrep/src/main.rs
+++ 12_cli_project/minigrep/src/main.rs
@@ -7,8 +7,9 @@ use minigrep::search;
 use minigrep::search_case_insensitive;
 
 // example usage:
-    // cargo run -- to poem.txt
-    // IGNORE_CASE=1 cargo run -- to poem.txt
+// Basic: `cargo run -- to poem.txt`
+// Ignore case with env var: `IGNORE_CASE=1 cargo run -- to poem.txt` ->
+// Turn off ignore case with flag (overrides env var): `cargo run -- to poem.txt --ignore_case=false`
 
 // main is only in charge of parsing the arguments and sending them to the run fn
 fn main() {
@@ -43,7 +44,13 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        // set ignore_case from env var
+        let mut ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        // check for --ignore_case flag (overrides env var)
+        if args.len() > 3 {
+            ignore_case = parse_flags(args);
+        }
 
         // create the config
         Ok(Config {
@@ -68,4 +75,26 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+// I added this
+fn parse_flags(args: &[String]) -> bool {
+    let flag = args[3].clone();
+
+    // ensure it's --ignore_case=true|false
+    let mut key_val_iterator = flag.split("=");
+
+    let key = key_val_iterator.next().unwrap();
+
+    if key != "--ignore_case" {
+        println!("invalid flag: {key}");
+        process::exit(1);
+    }
+
+    // get the bool and set ignore_case to it
+    match key_val_iterator.next().unwrap() {
+        "true" => true,
+        "false" => false,
+        _ => false,
+    }
 }
